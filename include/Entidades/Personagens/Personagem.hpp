98af165f50b623c04ab317847782ad0fd3bf91b5
Fernando Frare Vieira <fernandofrarevieira@gmail.com>
2024-12-28 06:18:52 -0300

    Adicionando velocidade na classe entidade

--- include/Entidades/Personagens/Personagem.hpp
+++ include/Entidades/Personagens/Personagem.hpp
@@ -12,7 +12,7 @@ namespace Entidades
                 int vida;
                 bool vivo;
             public:
-                Personagem(sf::Vector2f pos, sf::Vector2f tam, sf::Vector2f velocidade, ID identificador = vazio);
+                Personagem(sf::Vector2f pos, sf::Vector2f tam, sf::Vector2f vel, ID identificador = vazio);
                 ~Personagem();
                 virtual void executar() = 0;
                 virtual void desenhar() = 0;
