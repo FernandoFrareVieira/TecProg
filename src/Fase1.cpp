4a7f3f1d9580e5d1f961c7eaba5b2cc13c677ec6
FelipeReis96 <felrei700@gmail.com>
2025-01-28 22:19:34 -0300

    adicionando fase2

--- src/Fase1.cpp
+++ src/Fase1.cpp
@@ -11,7 +11,7 @@ namespace Fases
 
         // TODO - Arrumar a lógica da criaçao dos objetos
         carregarMapa("include/Tilemap/Tiles.json","include/Tilemap/Tileset.png");
-        jogador = new Entidades::Personagens::Jogador(sf::Vector2f(100.0f, 920.0f), sf::Vector2f(50.0f, 50.0f), sf::Vector2f(10.0f, 30.0f));
+        jogador = new Entidades::Personagens::Jogador(sf::Vector2f(100.0f, 920.0f), sf::Vector2f(50.0f, 50.0f), sf::Vector2f(5.0f, 30.0f));
         adicionarJogador(static_cast<Entidades::Entidade*>(jogador));
         pGC.setJogadores(&listaJogadores);
 
